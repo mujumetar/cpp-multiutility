b166f3d2571e13ae6a2e78a1aa7293fa4d3855c4
WebersIO <muzammilmetar82@gmail.com>
2024-10-15 18:46:08 +0530

    q1 solved

---
+++ q1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    float f, c;
+ 
+    cout << "Enter the temperature in Celsius : ";
+    cin >> c;
+    cout << endl;
+    f = (c * 9.0) / 5.0 + 32;
+    cout << endl;
+    cout << "********************************************************************************************************" <<endl;
+    cout << endl;
+    cout << "The temperature in Fahrenheit : " << f << endl;
+     cout << endl;
+    cout << "********************************************************************************************************" <<endl;
+    cout << endl;
+    return 0;
+}
